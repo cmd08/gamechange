05d13942131a74732a3817a1f9165decaf3dc12f
Chris Darby <me@chrisdarby.com>
2013-07-09 16:45:28 +0100

    Basic mock api data.

--- gamechange/bananas/__init__.py
+++ gamechange/bananas/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, abort, redirect, current_app
 from jinja2 import TemplateNotFound
+import time
+from json import dumps, loads
 
 bananas = Blueprint('bananas', __name__, template_folder='templates')
 app = current_app
@@ -24,4 +26,29 @@ def healthgraph_authorize():
 @bananas.route('/login')
 def login():
     print ""
+
+@bananas.route('/api/')
+def api_index():
+	return wrap_api_call()
+
+@bananas.route('/api/shop')
+def api_shop():
+	response = {'items' : [
+	{'name':'Coconut', 'cost':1, 'description': 'A coconut'},
+	{'name':'Shack', 'cost': 100, 'description': 'A slightly better house'}
+	]}
+	return wrap_api_call(response)
+
+@bananas.route('/api/user')
+def api_user():
+	response = {'username':'Joe Bloggs', 'bananas': '23'}
+	return wrap_api_call(response)
+
+def wrap_api_call(json=None):
+	wrapper = {'debug': True, 'api-version': 0.001, 'hostname': 'some-hostname:8888', 'system-time-millis': int(round(time.time() * 1000))}
+	if(json != None):
+		wrapper['data'] = json
+	return dumps(wrapper)
+
+
 
