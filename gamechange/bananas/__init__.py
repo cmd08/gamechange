211afc4458ebf1b6caf52161b2862c83651a3443
Aksat Shah <as10509@imperial.ac.uk>
2013-07-10 15:00:05 +0100

    modified pip file for beaker and requests - started integration of healthgraph api

--- gamechange/bananas/__init__.py
+++ gamechange/bananas/__init__.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, abort, redirect, current_app
+from flask import Blueprint, render_template, abort, redirect, current_app, jsonify
 from jinja2 import TemplateNotFound
+import healthgraph
 
 bananas = Blueprint('bananas', __name__, template_folder='templates')
 app = current_app
@@ -27,4 +28,17 @@ def login():
 
 @bananas.route('/healthgraph/test')
 def healthgraph_test():
-	return jsonify()
+	print ""
+
+def index():
+    sess = request.environ['beaker.session']
+    if sess.has_key('rk_access_token'):
+        bottle.redirect('healthgraph/test')
+    else:
+        rk_auth_mgr = healthgraph.AuthManager(conf['client_id'], conf['client_secret'], 
+                                          '/'.join((conf['baseurl'], 'login',)))
+        rk_auth_uri = rk_auth_mgr.get_login_url()
+        rk_button_img = rk_auth_mgr.get_login_button_url('blue', 'black', 300)
+        return bottle.template('index.html', {'rk_button_img': rk_button_img,
+                                              'rk_auth_uri': rk_auth_uri,})
+	# return jsonify()
