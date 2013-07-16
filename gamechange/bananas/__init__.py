48aca2719bdd1b0c5b10aa5abc0b137fff4dc6a7
Chris Darby <me@chrisdarby.com>
2013-07-16 15:20:28 +0100

    Extending the API a bit more.
    User login (Unauthenticated) method implemented to help Ashley keep
    getting useful JSON data for the front end. ShopItems now know about
    the shelter category.

--- gamechange/bananas/__init__.py
+++ gamechange/bananas/__init__.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, abort, redirect, current_app, jsonify, sessions, request
+from flask import Blueprint, render_template, abort, redirect, current_app, jsonify, session, request
 from jinja2 import TemplateNotFound
 import time
 import gamechange
-from gamechange.models import User, ShopItem
+from gamechange.models import User, ShopItem, Shelter
 
 bananas = Blueprint('bananas', __name__, template_folder='templates')
 app = current_app
@@ -18,10 +18,10 @@ def show(page):
 
 @bananas.route('/js/<page>')
 def js_show(page):
-	try:
-		return render_template('bananas/js/%s.html' % page)
-	except  TemplateNotFound:
-		abort(404)
+    try:
+        return render_template('bananas/js/%s.html' % page)
+    except  TemplateNotFound:
+        abort(404)
 
 @bananas.route('/de-authorize')
 def deauthorize_healthgraph_api():
@@ -37,37 +37,59 @@ def login():
 
 @bananas.route('/api/users', methods = ['GET'])
 def api_users():
-
-	json_list = [i.serialize for i in User.query.all()]
-	return wrap_api_call(json_list)
-	
+    json_list = [i.serialize for i in User.query.all()]
+    return wrap_api_call(json_list)
 
 @bananas.route('/api/')
 def api_index():
-	return wrap_api_call()
+    return wrap_api_call()
 
 @bananas.route('/api/shop', methods = ['GET'])
 def api_shop_get():
-	response = {'items' : [i.serialize for i in ShopItem.query.all()]}
-	# {'name':'Coconut', 'cost':1, 'description': 'A coconut'},
-	# {'name':'Shack', 'cost': 100, 'description': 'A slightly better house'}
-	# ]}
-	return wrap_api_call(response)
+    response = {'items' : [i.serialize for i in ShopItem.query.all()]}
+    # {'name':'Coconut', 'cost':1, 'description': 'A coconut'},
+    # {'name':'Shack', 'cost': 100, 'description': 'A slightly better house'}
+    # ]}
+    return wrap_api_call(response)
 
 @bananas.route('/api/shop', methods = ['POST'])
 def api_shop_post():
-	name = request.form['name']
-	description = request.form['description']
-	resp = {'name': name, 'description': description}
-	item = ShopItem(name, description)
-	gamechange.db.session.add(item)
-	gamechange.db.session.commit()
-	return wrap_api_call(resp)
+    name = request.form['name']
+    description = request.form['description']
+    type = request.form['type']
+    if type == 'shelter':
+        level = request.form['level']
+        image_url = request.form['image_url']
+        storage_space = request.form['storage_space']
+        food_decay_rate_multiplier = request.form['food_decay_rate_multiplier']
+        item = Shelter(name, description, level, image_url, storage_space, food_decay_rate_multiplier)
+        resp = item.serialize   
+    else :
+        item = ShopItem(name, description)
+        resp = item.serialize
+
+    gamechange.db.session.add(item)
+    gamechange.db.session.commit()
+    return wrap_api_call(resp)
 
 @bananas.route('/api/user',  methods = ['GET'])
 def api_user_get():
-	response = {'username':'Joe Bloggs', 'bananas': '23'}
-	return wrap_api_call(response)
+    if "username" not in session:
+        response = {'error': 'No logged in user'}
+        return wrap_api_call(response), 403
+
+    response = {'username':session['username'], 'bananas': session['bananas']}
+    return wrap_api_call(response)
+
+@bananas.route('/api/user/login', methods = ['POST'])
+def api_user_logi_post():
+    username = request.form['username']
+    password = request.form['password']
+    #Do some logic here to log the user in!
+    session['username'] = username
+    session['bananas'] = 0
+    response = {'username': username, 'bananas': session['bananas']}
+    return wrap_api_call(response)
 
 #post doesn't work yet! Returns 403!
 @bananas.route('/api/user', methods = ['POST'])
@@ -77,9 +99,8 @@ def api_user_post():
 	response = "lol"
 	return wrap_api_call(response)
 
-
 def wrap_api_call(json=None):
-	wrapper = {'_csrf_token': gamechange.generate_csrf_token(), 'debug': True, 'api-version': 0.1, 'hostname': app.config['SERVER_NAME'], 'system-time-millis': int(round(time.time() * 1000))}
-	if(json != None):
-		wrapper['data'] = json
-	return jsonify(wrapper)
+    wrapper = {'_csrf_token': gamechange.generate_csrf_token(), 'debug': True, 'api-version': 0.1, 'hostname': app.config['SERVER_NAME'], 'system-time-millis': int(round(time.time() * 1000))}
+    if(json != None):
+        wrapper['data'] = json
+        return jsonify(wrapper)
--- gamechange/models.py
+++ gamechange/models.py
@@ -4,22 +4,60 @@ from flask import jsonify
 db = SQLAlchemy()
 
 class ShopItem(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(120))
-	description = db.Column(db.String(512))
+    __tablename__ = "shop_item"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.String(512))
+    type = db.Column(db.String(50))
 
-	def __init__(self, name, description):
-		self.name = name
-		self.description = description
+    __mapper_args__ = {
+        'polymorphic_identity' : 'shop_item',
+        'polymorphic_on' : type
+    }
 
-	@property
-	def serialize(self):
-		"""Return object data in easily serializeable format"""
-		return {
-			'id'         : self.id,
-			'name'		: self.name,
-			'description': self.description
-		}
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id'         : self.id,
+            'name'		: self.name,
+            'description': self.description
+        }
+
+class Shelter(ShopItem):
+    level = db.Column(db.Integer)
+    image_url = db.Column(db.String(512))
+    storage_space = db.Column(db.Integer)
+    food_decay_rate_multiplier = db.Column(db.Integer)
+
+    def __init__(self, name, description, level, image_url, storage_space, food_decay_rate_multiplier):
+        self.name = name
+        self.description = description
+        self.level = level
+        self.image_url = image_url
+        self.storage_space = storage_space
+        self.food_decay_rate_multiplier = food_decay_rate_multiplier
+    
+    __mapper_args__ = {
+        'polymorphic_identity' : 'shelter'
+    }
+
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'description': self.description,
+            'level' : self.level,
+            'image_url' : self.image_url,
+            'storage_space' : self.storage_space,
+            'food_decay_rate_multiplier' : self.food_decay_rate_multiplier
+        }
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
