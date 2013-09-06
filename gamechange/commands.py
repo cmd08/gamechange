124ac737e6bd61a73d69d4ee7883e63a8286b479
Aksat Shah <as10509@imperial.ac.uk>
2013-09-06 17:56:15 +0100

    correction to previous message on cron command - fixes #110
    > To run script in cron use the command:
    	> crontab -e
    	> 0 0 * * * cd /home/aksat/python/gamechange/gamechange/ && export PYTHONPATH=/home/aksat/python/gamechange && export FLASK_CONFIG=/home/aksat/python/gamechange/conf && /home/aksat/python/gamechange/bin/python /home/aksat/python/gamechange/gamechange/commands.py reduce_health
    
    > Currently health is default 100 - once levels are done we should pull from shelter table

--- gamechange/commands.py
+++ gamechange/commands.py
@@ -2,6 +2,7 @@ from flask.ext.script import Manager
 from flask import Flask
 from gamechange.models import User, db
 
+
 app = Flask(__name__)
 app.config.from_envvar('FLASK_CONFIG')
 db.init_app(app)
